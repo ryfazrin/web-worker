1c9dc79e1378d668a0cb281f13a7d922b1f2ce63
ryfazrin <riansera515@gmail.com>
2022-10-14 03:48:27 +0700

    updated: add info for worker

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ ASYNC_RUN.addEventListener('click', _ => {
   setTimeout(() => runAsync(ASYNC_INFO_ID), timeBetween);
 });
 
+WORKER_INFO_ID.innerHTML += `
+<li>'Execute renning' item run in <b>Console.log</b></li>
+<li>
+If you want to see the actual execution time. Turn on <b>Inspect Devtools</b>.
+In chrome <b>Ctrl + Shift + i</b>. Then visit the tab <b>console</b>.
+</li>
+`;
 WORKER_RUN.addEventListener('click', _ => {
-  WORKER_INFO_ID.innerHTML += `<li>Item Count in Console.log.</li>`;
   runWorker(WORKER_INFO_ID);
 });
